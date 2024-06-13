d68e681b3c72fd445e6cfe6ba13e0c6d9c2e15bd
kradib <radibkar32@gmail.com>
2024-06-13 13:46:34 +0530

    INitial commit for bolierplate code

---
+++ src/main/java/com/example/moview_review_system/MoviewReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.moview_review_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviewReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviewReviewSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moview_review_system/MoviewReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moview_review_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
